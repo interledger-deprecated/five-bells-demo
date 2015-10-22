097fb890c9e3f3100cfbcd5d0d869e796b5ad607
Evan Schwartz <evan.mark.schwartz@gmail.com>
2015-10-21 17:25:22 -0700

    [TASK] Refactor demo parameters into constructor

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 'use strict'
 
-const Demo = require('./lib/demo')
+const Demo = require('./lib/demo').Demo
 
-const demo = new Demo()
+let numLedgers = 8
+let numTraders = 7
+
+if (process.env.DEMO_NUM_LEDGERS) {
+  numLedgers = parseInt(process.env.DEMO_NUM_LEDGERS, 10)
+}
+
+if (process.env.DEMO_NUM_TRADERS) {
+  numTraders = parseInt(process.env.DEMO_NUM_TRADERS, 10)
+}
+
+const demo = new Demo({
+  numLedgers,
+  numTraders
+})
 demo.start()
