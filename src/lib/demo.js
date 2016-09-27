b3c7c1d1a65cbbc6eea109a40554bf3e31d3b7cc
sentientwaffle <sentientwaffle@gmail.com>
2016-09-27 10:09:57 -0700

    fix: five-bells-service-manager; update deps
    Fixes #34

--- src/lib/demo.js
+++ src/lib/demo.js
@@ -1,7 +1,9 @@
 'use strict'
 
+const co = require('co')
 const path = require('path')
 const randomgraph = require('randomgraph')
+const ServiceManager = require('five-bells-service-manager')
 
 const connectorNames = [
   'mark', 'mary', 'martin', 'millie',
@@ -48,6 +50,9 @@ class Demo {
   constructor (opts) {
     const _this = this
 
+    this.services = new ServiceManager(
+      path.resolve(__dirname, '../../node_modules'),
+      path.resolve(__dirname, '../../data'))
     this.adminUser = opts.adminUser
     this.adminPass = opts.adminPass
 
@@ -76,51 +81,57 @@ class Demo {
     for (let i = 0; i < this.numConnectors; i++) {
       this.connectorEdges[i] = []
     }
+    this.ledgerHosts = {}
     this.graph.edges.forEach(function (edge, i) {
-      edge.source_currency = currencies[edge.source % currencies.length].code
-      edge.target_currency = currencies[edge.target % currencies.length].code
-      edge.source = 'http://localhost:' + (3001 + edge.source)
-      edge.target = 'http://localhost:' + (3001 + edge.target)
+      const source = edge.source
+      const target = edge.target
+      edge.source_currency = currencies[source % currencies.length].code
+      edge.target_currency = currencies[target % currencies.length].code
+      edge.source = 'demo.ledger' + source + '.'
+      edge.target = 'demo.ledger' + target + '.'
+      this.ledgerHosts[edge.source] = 'http://localhost:' + (3000 + source)
+      this.ledgerHosts[edge.target] = 'http://localhost:' + (3000 + target)
       _this.connectorEdges[i % _this.numConnectors].push(edge)
+    }, this)
+  }
+
+  start () {
+    return co.wrap(this._start).call(this)
+  }
+
+  * _start () {
+    for (let i = 0; i < this.numLedgers; i++) {
+      yield this.startLedger('demo.ledger' + i + '.', 3000 + i)
+    }
+
+    for (let i = 0; i < this.numConnectors; i++) {
+      const connector = connectorNames[i] || 'connector' + i
+      yield this.startConnector(connector, 4000 + i, this.connectorEdges[i])
+    }
+    yield this.services.startVisualization(5000)
+  }
+
+  * startLedger (ledger, port) {
+    yield this.services.startLedger(ledger, port, {})
+    yield this.services.updateAccount(ledger, 'alice', {balance: '1000'})
+    yield this.services.updateAccount(ledger, 'bob', {balance: '1000'})
+  }
+
+  * startConnector (connector, port, edges) {
+    for (const edge of edges) {
+      yield this.services.updateAccount(edge.source, connector, {balance: '1000'})
+      yield this.services.updateAccount(edge.target, connector, {balance: '1000'})
+    }
+    yield this.services.startConnector(port, {
+      pairs: this.edgesToPairs(edges),
+      credentials: this.edgesToCredentials(edges, connector),
+      backend: 'fixerio'
     })
   }
 
-  createLedger (name, port) {
-    const currency = currencies[Math.floor(Math.random() * currencies.length)]
-    const dbUri = process.env.LEDGER_DB_URI || 'sqlite://' + path.resolve(__dirname, '../../data/' + name + '.sqlite')
-    return {
-      env: {
-        PATH: process.env.PATH,
-        LEDGER_DB_URI: dbUri,
-        LEDGER_DB_SYNC: true,
-        LEDGER_HOSTNAME: 'localhost',
-        LEDGER_PORT: port,
-        LEDGER_ADMIN_USER: this.adminUser,
-        LEDGER_ADMIN_PASS: this.adminPass,
-        LEDGER_CURRENCY_CODE: currency.code,
-        LEDGER_CURRENCY_SYMBOL: currency.symbol
-      },
-      cwd: './node_modules/five-bells-ledger',
-      cmd: this.npmPrefix + ' start -- --color',
-      waitFor: 'listening',
-      alias: 'ledger-' + name
-    }
-  }
-
-  static makeCredentials (ledger, name) {
-    return {
-      account_uri: ledger + '/accounts/' + encodeURIComponent(name),
-      username: name,
-      password: name
-    }
-  }
-
-  createConnector (name, port, edges) {
-    let creds = {}
-    let pairs = []
-    for (let edge of edges) {
-      creds[edge.source] = Demo.makeCredentials(edge.source, name)
-      creds[edge.target] = Demo.makeCredentials(edge.target, name)
+  edgesToPairs (edges) {
+    const pairs = []
+    for (const edge of edges) {
       pairs.push([
         edge.source_currency + '@' + edge.source,
         edge.target_currency + '@' + edge.target
@@ -130,92 +141,24 @@ class Demo {
         edge.source_currency + '@' + edge.source
       ])
     }
-
-    return {
-      env: {
-        CONNECTOR_CREDENTIALS: JSON.stringify(creds),
-        CONNECTOR_DEBUG_AUTOFUND: '1',
-        CONNECTOR_PAIRS: JSON.stringify(pairs),
-        CONNECTOR_MAX_HOLD_TIME: 600,
-        CONNECTOR_SPREAD: 0,
-        PATH: process.env.PATH,
-        CONNECTOR_HOSTNAME: 'localhost',
-        CONNECTOR_PORT: port,
-        CONNECTOR_ADMIN_USER: this.adminUser,
-        CONNECTOR_ADMIN_PASS: this.adminPass
-      },
-      cwd: './node_modules/five-bells-connector',
-      cmd: this.npmPrefix + ' start -- --color',
-      alias: 'connector-' + name
-    }
+    return pairs
   }
 
-  createVisualization (port, ledger) {
-    return {
-      env: {
-        PATH: process.env.PATH,
-        CRAWLER_INITIAL_LEDGERS: ledger,
-        VISUALIZATION_RECRAWL_INTERVAL: 30000,
-        HOSTNAME: 'localhost',
-        PORT: port,
-        ADMIN_USER: this.adminUser,
-        ADMIN_PASS: this.adminPass
-      },
-      cwd: './node_modules/five-bells-visualization',
-      cmd: this.npmPrefix + ' start -- --color',
-      alias: 'visualization'
+  edgesToCredentials (edges, connectorName) {
+    const creds = {}
+    for (const edge of edges) {
+      creds[edge.source] = this.makeCredentials(edge.source, connectorName)
+      creds[edge.target] = this.makeCredentials(edge.target, connectorName)
     }
+    return creds
   }
 
-  createNotary (port) {
-    const dbUri = process.env.NOTARY_DB_URI || 'sqlite://' + path.resolve(__dirname, '../../data/notary.sqlite')
+  makeCredentials (ledger, name) {
     return {
-      env: {
-        PATH: process.env.PATH,
-        NOTARY_DB_URI: dbUri,
-        NOTARY_HOSTNAME: 'localhost',
-        NOTARY_PORT: port
-      },
-      cwd: './node_modules/five-bells-notary',
-      cmd: this.npmPrefix + ' start -- --color',
-      alias: 'notary'
+      account: this.ledgerHosts[ledger] + '/accounts/' + encodeURIComponent(name),
+      username: name,
+      password: name
     }
-  }
-
-  createAccount (ledger, name) {
-    return {
-      env: {
-        PATH: process.env.PATH,
-        LEDGER: ledger,
-        USERNAME: name,
-        ADMIN_USER: this.adminUser,
-        ADMIN_PASS: this.adminPass
-      },
-      cwd: './',
-      cmd: './scripts/create-account.js',
-      alias: 'create-account-' + name
-    }
-  }
-
-  start () {
-    const multiplexer = require('multiplexer')
-    const processes = []
-    const accounts = []
-    for (let i = 1; i <= this.numLedgers; i++) {
-      let port = 3000 + i
-      processes.push(this.createLedger('ledger' + i, port))
-      accounts.push(this.createAccount('http://localhost:' + port, 'alice'))
-      accounts.push(this.createAccount('http://localhost:' + port, 'bob'))
-    }
-
-    for (let i = 0; i < this.numConnectors; i++) {
-      processes.push(this.createConnector(connectorNames[i] || 'connector' + i, 4001 + i, this.connectorEdges[i]))
-    }
-
-    multiplexer(processes.concat([
-      this.createNotary(6001),
-      this.createVisualization(5001, 'http://localhost:3001')
-    ], accounts))
   }
 }
 
